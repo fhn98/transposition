642580d751eca380ac799bf2749322d757b1f0e6
mrsamsami <mohammad.r.samsami@gmail.com>
2018-12-31 11:15:47 +0330

    simple fully connected

--- lstm-prgph-encoder-fc-decoder/classifier.py
+++ lstm-prgph-encoder-fc-decoder/classifier.py
@@ -4,34 +4,34 @@ import numpy as np
 
 
 class Classifier(Model):
-    
-    def __init__(self, name, num_classes = 1, hidden_dim = 64, dropout = 0.2):
+
+    def __init__(self, name, output_size=1, hidden_dim=64, dropout=0.2):
         """
         This classifier determines if paragraphs are forward or backward
-        :param name: name of the classifier
-        :param num_classes: dimension of output
-        :param hidden_dim: dimension of hidden layer
-        :param dropout: dropout value
+        Args:
+            name: the name of the classifier
+            num_classes: dimension of output
+            hidden_dim: dimension of hidden layer
+            dropout: dropout value
         """
-        
+
         super(Classifier, self).__init__()
         self._name = name
-        self.num_classes = num_classes
+        self.output_size = output_size
         self.hidden_dim = hidden_dim
-        self.dropout = layers.Dropout(dropout)
-        self.layer1 = layers.Dense(self.hidden_dim, activation = 'relu') 
-        self.layer2 = layers.Dense(self.num_classes, activation = 'tanh')
-    
+        self.dropout = layers.Dropout(dropout, name=name + '/Dropout')
+        self.layer1 = layers.Dense(self.hidden_dim, activation='relu', name=name + '/Dense1')
+        self.layer2 = layers.Dense(self.output_size, activation='tanh', name=name + '/Dense2')
+
     @property
     def variables(self):
         return [x.variables for x in self._layers]
-    
+
     @property
     def trainable_variables(self):
         return [var for var in self._variables if self._trainable_variables[var]]
-    
-    
-    def call(self, inputs, is_training = True):
+
+    def call(self, inputs, is_training=True):
         y = self.layer1(inputs)
         if is_training:
             y = self.dropout(y)
